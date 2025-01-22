edf2179cb197118a2ed10bc22d1953f95e55858b
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-01-22 07:57:28 -0700

    Add some dummy code inside add on

--- myaddon/__init__.py
+++ myaddon/__init__.py
@@ -6,8 +6,29 @@ from aqt.utils import showInfo, qconnect
 from aqt.qt import *
 
 def testFunction():
+  # Get the total number of card inside Anki
   cardCount = mw.col.cardCount()
   showInfo("Card count: " + str(cardCount))
+  # Try to find a due card
+  card = mw.col.sched.getCard()
+  if not card:
+    # The current deck is done
+    print("The deck is done! You have done a good job today")
+  else:
+    note = card.note()
+    for (name,value) in note.items():
+      note[name] = value + " new (Added by Bao addons"
+  # Get card IDs for note with tag x
+  ids = mw.col.find_cards("tag:x")
+
+  # Print out all the question and answer of those ids
+  for id in ids:
+    card = mw.col.sched.getCard(id)
+    question = card.question()
+    answer = card.answer()
+    print(question, answer)
+
+
 
 
 #mw mean main window
